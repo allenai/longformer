0a07daf8dd0cf2ca24592f4006338b2568cca23b
Iz Beltagy <beltagy@allenai.org>
2020-07-22 20:16:38 +0000

    wip

---
+++ scripts/test_tpu.py
@@ -0,0 +1,57 @@
+import os
+import torch
+from torch.utils.data import DataLoader, Dataset
+from transformers import AutoModel
+import pytorch_lightning as pl
+
+class CoolDataset(Dataset):
+  def __len__(self):
+      return 128 * 128
+
+  def __getitem__(self, idx):
+      return torch.tensor([1, 2, 3, 4,] * 128), torch.tensor([1, 1, 1, 1] * 128)
+
+class CoolSystem(pl.LightningModule):
+
+    def __init__(self):
+        super().__init__()
+
+        # self.model = AutoModel.from_pretrained('allenai/longformer-base-4096')
+        self.model = AutoModel.from_pretrained('bert-base-uncased')
+
+    def forward(self, x, y):
+        return self.model(x, attention_mask=None)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x, y)
+        loss = y_hat[0].sum() #  F.cross_entropy(y_hat, y)
+        tensorboard_logs = {'train_loss': loss}
+        return {'loss': loss, 'log': tensorboard_logs}
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x, y)
+        val_loss = y_hat[0].sum() #  F.cross_entropy(y_hat, y)
+        return {'val_loss': val_loss}
+
+    def validation_epoch_end(self, outputs):
+        avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
+        tensorboard_logs = {'val_loss': avg_loss}
+        return {'val_loss': avg_loss, 'log': tensorboard_logs}
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=0.001)
+
+    def train_dataloader(self):
+        loader = DataLoader(CoolDataset(), batch_size=1, num_workers=0)
+        return loader
+
+    def val_dataloader(self):
+        loader = DataLoader(CoolDataset(), batch_size=1, num_workers=0)
+        return loader
+
+if __name__ == '__main__':
+    model = CoolSystem()
+    trainer = pl.Trainer(num_tpu_cores=1, progress_bar_refresh_rate=1, max_epochs=10, num_sanity_val_steps=0)
+    trainer.fit(model)
