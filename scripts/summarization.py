b78384a826635a2bc656295ffbd2beb17e44fb4e
Iz Beltagy <beltagy@allenai.org>
2020-09-07 04:15:32 +0000

    wip

--- scripts/summarization.py
+++ scripts/summarization.py
@@ -37,7 +37,14 @@ class SummarizationDataset(Dataset):
 
     @staticmethod
     def collate_fn(batch):
-        pad_token_id = 1  # AutoTokenizer.from_pretrained('facebook/bart-base').pad_token_id
+        # A hack to know if this is bart or pegasus. DDP doesn't like global variables nor class-level memebr variables
+        if batch[0][0][-1].item() == 2:
+            pad_token_id = 1  # AutoTokenizer.from_pretrained('facebook/bart-base').pad_token_id
+        elif batch[0][0][-1].item() == 1:
+            pad_token_id = 0  # AutoTokenizer.from_pretrained('google/pegasus-large').pad_token_id
+        else:
+            assert False
+
         input_ids, output_ids = list(zip(*batch))
         input_ids = torch.nn.utils.rnn.pad_sequence(input_ids, batch_first=True, padding_value=pad_token_id)
         output_ids = torch.nn.utils.rnn.pad_sequence(output_ids, batch_first=True, padding_value=pad_token_id)
@@ -76,7 +83,6 @@ class Summarizer(pl.LightningModule):
         decoder_attention_mask = (decoder_input_ids != self.tokenizer.pad_token_id)
         labels = output_ids[:, 1:].clone()
         labels[labels == self.tokenizer.pad_token_id] = -100
-
         outputs = self.model(
                 input_ids,
                 attention_mask=attention_mask,
@@ -108,8 +114,8 @@ class Summarizer(pl.LightningModule):
         generated_ids = self.model.generate(input_ids=input_ids, attention_mask=attention_mask,
                                             use_cache=True, max_length=self.args.max_output_len,
                                             num_beams=1)
-        generated_str = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
-        gold_str = self.tokenizer.batch_decode(output_ids, skip_special_tokens=True)
+        generated_str = self.tokenizer.batch_decode(generated_ids.tolist(), skip_special_tokens=True)
+        gold_str = self.tokenizer.batch_decode(output_ids.tolist(), skip_special_tokens=True)
         scorer = rouge_scorer.RougeScorer(rouge_types=['rouge1', 'rouge2', 'rougeL'], use_stemmer=False)
         rouge1 = rouge2 = rougel = 0.0
         for ref, pred in zip(gold_str, generated_str):
