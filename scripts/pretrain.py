be86e134eedddc310412ea1717b46b412d728751
Arman Cohan <armanc@allenai.org>
2020-11-02 16:27:33 -0800

    global_tokens at data loader

--- scripts/pretrain.py
+++ scripts/pretrain.py
@@ -43,18 +43,21 @@ else:
 
 
 class MMapTextDataset(Dataset):
-    def __init__(self, mmap_filename, chunk_size, bos_token_id, eos_token_id):
+    def __init__(self, mmap_filename, chunk_size, bos_token_id, eos_token_id, num_global_tokens=0):
         # `chunk_size - 2` to reserve space for <s> and </s>
-        self.num_instances = np.memmap(mmap_filename, mode='r', dtype=np.uint16).shape[0] // (chunk_size - 2)
+        self._chunk_size = chunk_size - 2
+        # add additional <s> for global attention, default is 1 bos_token as in bert
+        self._chunk_size -= num_global_tokens
+        self.num_instances = np.memmap(mmap_filename, mode='r', dtype=np.uint16).shape[0] // (self._chunk_size - 2)
         # defer loading the token_ids memmap until after the first __getitem__ call.
         # when spawning new processes for ddp, there is a hard limit in python < 3.8 that
         # pickle files need to be < 4GB. By waiting until after the first __getitem__ we
         # don't have to pickle the memmap
         self.token_ids = None
         self._mmap_filename = mmap_filename
-        self._chunk_size = chunk_size
         self._bos_token_id = bos_token_id
         self._eos_token_id = eos_token_id
+        self.num_global_tokens = num_global_tokens
 
     def __len__(self):
         return self.num_instances
@@ -64,7 +67,8 @@ class MMapTextDataset(Dataset):
             self.token_ids = np.memmap(self._mmap_filename, mode='r', dtype=np.uint16)
         from_index = i * (self._chunk_size - 2)
         to_index = (i + 1) * (self._chunk_size - 2)
-        data = np.concatenate(([self._bos_token_id], self.token_ids[from_index:to_index], [self._eos_token_id]))
+        bos_tokens = [self._bos_token_id for _ in range(self.num_global_tokens + 1)]
+        data = np.concatenate((bos_tokens, self.token_ids[from_index:to_index], [self._eos_token_id]))
         return torch.tensor(data, dtype=torch.long)
 
 class Pretrainer(ptl.LightningModule):
@@ -189,7 +193,8 @@ class Pretrainer(ptl.LightningModule):
 
     def _get_loader(self, fname, is_train):
         dataset = MMapTextDataset(fname, chunk_size=self.args.seqlen,
-                                  bos_token_id=self.bos_token_id, eos_token_id=self.eos_token_id)
+                                  bos_token_id=self.bos_token_id, eos_token_id=self.eos_token_id,
+                                  num_global_tokens=self.args.global_tokens)
 
         # TODO: consider `replace_sampler_ddp=True` and removing the following if statement
         if self.trainer.use_ddp:
