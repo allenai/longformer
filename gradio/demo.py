7d67c62a797d25ba0471cb9db5ac930321026a7b
AK391 <81195143+AK391@users.noreply.github.com>
2021-06-11 23:49:06 -0400

    add examples

--- gradio/demo.py
+++ gradio/demo.py
@@ -1,3 +1,6 @@
 import gradio as gr
 
-gr.Interface.load("huggingface/allenai/longformer-large-4096-finetuned-triviaqa").launch()
+gr.Interface.load("huggingface/allenai/longformer-large-4096-finetuned-triviaqa", examples=[
+  ["My name is Wolfgang and I live in Berlin", "Where do I live?"],
+  ["The capital of France is Paris", "What is the capital of France?"]
+]).launch()
