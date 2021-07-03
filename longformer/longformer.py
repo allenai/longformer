4aec55e884a9954cab5381268d1bd9d97231c5ec
Herais <48284363+Herais@users.noreply.github.com>
2021-07-02 23:48:18 -0700

    Update longformer.py
    Update
    "from transformers.models.roberta.modeling_roberta import RobertaConfig, RobertaModel, RobertaForMaskedLM"
    from
    "from transformers.modeling_roberta import RobertaConfig, RobertaModel, RobertaForMaskedLM"
    to keep up with latest transformers

--- longformer/longformer.py
+++ longformer/longformer.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 from longformer.diagonaled_mm_tvm import diagonaled_mm as diagonaled_mm_tvm, mask_invalid_locations
 from longformer.sliding_chunks import sliding_chunks_matmul_qk, sliding_chunks_matmul_pv
 from longformer.sliding_chunks import sliding_chunks_no_overlap_matmul_qk, sliding_chunks_no_overlap_matmul_pv
-from transformers.modeling_roberta import RobertaConfig, RobertaModel, RobertaForMaskedLM
+from transformers.models.roberta.modeling_roberta import RobertaConfig, RobertaModel, RobertaForMaskedLM
 
 
 class Longformer(RobertaModel):
